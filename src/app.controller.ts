45c1e44e7a1153581f7ef751713039c0ac279eea
akai <kai150036@163.com>
2025-01-04 23:05:57 +0800

    feat: nestjs 中集成ioredis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import { Controller, Get, Query } from '@nestjs/common';
+import Redis from 'ioredis';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  @Get()
+  async getHello(@Query('key') key: string) {
+    await this.redis.set('key', key || 'default', 'EX', 60);
+    const redisData = await this.redis.get('key');
+    return { redisData };
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,31 @@ import { ConfigModule } from './common/config/config.module';
 import { LoggerModule } from './common/Logger/Logger.module';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
-  imports: [ConfigModule, LoggerModule, DatabaseModule, UserModule],
+  imports: [
+    // RedisModule.forRoot({
+    //   type: 'single',
+    //   url: 'redis://localhost:6379',
+    //   options: {
+    //     password: 'example',
+    //   },
+    // }),
+    RedisModule.forRootAsync({
+      useFactory: () => ({
+        type: 'single',
+        url: 'redis://localhost:6379',
+        options: {
+          password: 'example',
+        },
+      }),
+    }),
+    ConfigModule,
+    LoggerModule,
+    DatabaseModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
